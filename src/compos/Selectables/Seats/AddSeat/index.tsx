dcbf24b360580291b110a924babbfa780e63c654
Blake Preston <note2bp@protonmail.com>
2024-08-20 13:34:48 -0600

    working tooltip component

--- src/compos/Selectables/Seats/AddSeat/index.tsx
+++ src/compos/Selectables/Seats/AddSeat/index.tsx
@@ -1,3 +1,4 @@
+import Tooltip from "../../../Tooltip";
 import "./style.css";
 
 const AddSeat: React.FC<{
@@ -5,8 +6,12 @@ const AddSeat: React.FC<{
 }> = ({ addHandler }) => {
    return (
       <>
-         <div className="add-seat no-print" onClick={addHandler}>
-            <img src="/plus.webp" width={"16px"} />
+         <div className="add-seat-wrapper">
+            <Tooltip content="Add Rail Seat 🪑" direction="top">
+               <div className="add-seat no-print" onClick={addHandler}>
+                  <img src="/plus.webp" width={"16px"} />
+               </div>
+            </Tooltip>
          </div>
       </>
    );
---
+++ src/compos/Tooltip/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./style.css";
+
+interface TooltipProps {
+   delay?: number;
+   direction?: "top" | "right" | "bottom" | "left";
+   content: React.ReactNode;
+   children: React.ReactNode;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+   delay,
+   direction,
+   content,
+   children,
+}) => {
+   let timeout: NodeJS.Timeout;
+   const [active, setActive] = useState(false);
+
+   const showTip = () => {
+      timeout = setTimeout(() => {
+         setActive(true);
+      }, delay || 400);
+   };
+
+   const hideTip = () => {
+      clearTimeout(timeout);
+      setActive(false);
+   };
+
+   return (
+      <div
+         className="Tooltip-Wrapper"
+         onMouseEnter={showTip}
+         onMouseLeave={hideTip}
+      >
+         {children}
+         {active && (
+            <div className={`Tooltip-Tip ${direction || "top"}`}>{content}</div>
+         )}
+      </div>
+   );
+};
+
+export default Tooltip;
